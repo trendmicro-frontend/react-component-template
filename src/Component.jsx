813315548a63f0810f01be05518b652775aa7f00
Cheton Wu <cheton@gmail.com>
2016-12-13 00:36:36 +0800

    Update template

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import shallowCompare from 'react-addons-shallow-compare';
 import styles from './index.styl';
 
 export default class extends Component {
@@ -12,6 +13,9 @@ export default class extends Component {
     actions = {
     };
 
+    shouldComponentUpdate(nextProps, nextState) {
+        return shallowCompare(this, nextProps, nextState);
+    }
     render() {
         return (
             <div className={styles.componentTitle}>Hello</div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-var fs = require('fs');
 var pkg = require('./package.json');
 var bower = require('./bower.json');
 var path = require('path');
@@ -13,19 +12,6 @@ var banner = [
     pkg.homepage
 ].join(' | ');
 var localClassPrefix = publicname.replace(/^react-/, ''); // Strip out "react-" from publicname
-
-// Update bower.json
-Object.keys(bower).forEach((key) => {
-    bower[key] = pkg[key] || bower[key];
-});
-bower.authors = pkg.contributors.map((author) => {
-    return {
-        name: author.name,
-        email: author.email,
-        homepage: author.url
-    };
-});
-fs.writeFileSync('bower.json', JSON.stringify(bower, null, 2), 'utf8');
 
 module.exports = {
     devtool: 'source-map',
