8d5ba07f467ebd63599bacd4f3b784b5642ceff9
Cheton Wu <cheton@gmail.com>
2017-02-10 19:28:38 +0800

    Add support for responsive nav

--- examples/index.jsx
+++ examples/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Nav from './nav';
 import Component from '../src';
 
 class App extends React.Component {
     render() {
+        const name = 'React Component';
+        const url = 'https://github.com/trendmicro-frontend/react-component';
+
         return (
-            <Component />
+            <div>
+                <Nav name={name} url={url} />
+                <div className="container-fluid">
+                    <Component />
+                </div>
+            </div>
         );
     }
 }
