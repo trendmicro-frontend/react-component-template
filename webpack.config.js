6e9d986fb31dfe1fdfca313742e92cae02d6f3ca
Tina C Lin (RD-TW) <tina_c_lin@trend.com.tw>
2017-02-10 10:36:27 +0800

    Adjust config of webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -29,18 +29,18 @@ module.exports = {
             // http://survivejs.com/webpack_react/linting_in_webpack/
             {
                 test: /\.jsx?$/,
-                use: 'eslint-loader',
+                loader: 'eslint-loader',
                 enforce: 'pre',
                 exclude: /node_modules/
             },
             {
                 test: /\.styl$/,
-                use: 'stylint-loader',
+                loader: 'stylint-loader',
                 enforce: 'pre'
             },
             {
                 test: /\.jsx?$/,
-                use: 'babel-loader',
+                loader: 'babel-loader',
                 exclude: /(node_modules|bower_components)/
             },
             {
@@ -52,7 +52,7 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: 'style-loader!css-loader'
+                loader: 'style-loader!css-loader'
             }
         ]
     },
