9e20c2d11bcb0fb3edef9ce5a59fd01e5fbf6460
Cheton Wu <cheton@gmail.com>
2017-02-23 15:59:51 +0800

    Update examples

--- examples/index.jsx
+++ examples/index.jsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import Nav from './Nav';
 import Section from './Section';
 import Component from '../src';
-import styles from './index.styl';
+//import styles from './index.styl';
 
 class App extends React.Component {
     render() {
@@ -15,35 +15,33 @@ class App extends React.Component {
         return (
             <div>
                 <Nav name={name} url={url} />
-                <div className={styles.container}>
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col-md-12">
-                                <Section className="row-md-6">
-                                    <h2>Title #1</h2>
-                                    <Component>
-                                        Content #1
-                                    </Component>
-                                </Section>
-                            </div>
+                <div className="container-fluid" style={{ padding: '20px 20px 0' }}>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <Section className="row-md-6">
+                                <h2>Title #1</h2>
+                                <Component>
+                                    Content #1
+                                </Component>
+                            </Section>
                         </div>
-                        <div className="row">
-                            <div className="col-md-6">
-                                <Section className="row-md-3">
-                                    <h2>Title #2</h2>
-                                    <Component>
-                                        Content #2
-                                    </Component>
-                                </Section>
-                            </div>
-                            <div className="col-md-6">
-                                <Section className="row-md-3">
-                                    <h2>Title #3</h2>
-                                    <Component>
-                                        Content #3
-                                    </Component>
-                                </Section>
-                            </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <Section className="row-md-3">
+                                <h2>Title #2</h2>
+                                <Component>
+                                    Content #2
+                                </Component>
+                            </Section>
+                        </div>
+                        <div className="col-md-6">
+                            <Section className="row-md-3">
+                                <h2>Title #3</h2>
+                                <Component>
+                                    Content #3
+                                </Component>
+                            </Section>
                         </div>
                     </div>
                 </div>
